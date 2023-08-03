4cd3e205695165cbcd64e78e15697dcb8757c858
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-03 16:35:14 +0200

    Task 8 file

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+function submitForm(event) {
+  event.preventDefault();
+  let elements = {};
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields should be filled in");
+  }
+
+  elements = { email: email.value, password: password.value };
+  console.log(elements);
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", submitForm);
