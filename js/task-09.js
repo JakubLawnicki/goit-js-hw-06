51c011958485e94f9c0099a98b9357fb19acfc7e
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-03 16:54:55 +0200

    Task 9 file

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonColor = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+const changeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+};
+
+buttonColor.addEventListener("click", changeColor);
