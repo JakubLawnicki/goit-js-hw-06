0e8c4218e6936b9b6f2214592a0f04734e12894b
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-04 22:10:20 +0200

    Task 10 file

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,27 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    boxes.append(document.createElement("div"));
+    size += 10;
+    boxes.lastElementChild.style.width = `${size}px`;
+    boxes.lastElementChild.style.height = `${size}px`;
+    boxes.lastElementChild.style.backgroundColor = getRandomHexColor();
+  }
+
+  return boxes;
+}
+
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy");
+
+input.addEventListener("input", () => input.value);
+destroyBtn.addEventListener("click", () => boxes.replaceChildren());
+createBtn.addEventListener("click", () => {
+  return createBoxes(input.value);
+});
