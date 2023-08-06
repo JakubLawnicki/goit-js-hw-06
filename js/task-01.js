3847aeb6a91bdb5fd8129afffe44f21b8c51cce3
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-06 21:49:55 +0200

    Task 1 revised

--- js/task-01.js
+++ js/task-01.js
@@ -1,41 +1,12 @@
 const mainList = document.querySelector("#categories");
 
-const items = document.querySelectorAll(".item");
+const item = document.querySelectorAll(".item");
 
-const firstCategory = mainList.firstElementChild;
+const items = Array.from(mainList.children);
 
-const firstCategoryHeading = firstCategory.querySelector("h2");
+console.log("Number of categories:", mainList.querySelectorAll(".item").length);
 
-const firstCategoryList = firstCategory.querySelector("ul");
-
-const firstCategoryListElements = firstCategoryList.children;
-
-const firstCategoryHeadingText = firstCategoryHeading.textContent;
-
-const secondCategory = firstCategory.nextElementSibling;
-
-const secondCategoryHeading = secondCategory.querySelector("h2");
-
-const secondCategoryHeadingText = secondCategoryHeading.textContent;
-
-const secondCategoryList = secondCategory.querySelector("ul");
-
-const secondCategoryListElements = secondCategoryList.children;
-
-const thirdCategory = mainList.lastElementChild;
-
-const thirdCategoryHeading = thirdCategory.querySelector("h2");
-
-const thirdCategoryHeadingText = thirdCategoryHeading.textContent;
-
-const thirdCategoryList = thirdCategory.querySelector("ul");
-
-const thirdCategoryListElements = thirdCategoryList.children;
-
-console.log("Number of categories:", items.length);
-console.log("Category:", firstCategoryHeadingText);
-console.log("Elements:", firstCategoryListElements.length);
-console.log("Category:", secondCategoryHeadingText);
-console.log("Elements:", secondCategoryListElements.length);
-console.log("Category:", thirdCategoryHeadingText);
-console.log("Elements:", thirdCategoryListElements.length);
+items.forEach((item) => {
+  console.log("Category:", item.querySelector("h2").textContent);
+  console.log("Elements:", item.querySelectorAll("li").length);
+});
