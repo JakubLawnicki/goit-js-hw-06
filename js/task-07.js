ad1a4083d33f52a6358280059f60447019b04457
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-08-11 21:12:09 +0200

    Task 7 updated

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,9 @@
 const scrollbar = document.querySelector("#font-size-control");
 const textOutput = document.querySelector("#text");
 
+scrollbar.value = 16;
+textOutput.style.fontSize = "16px";
+
 const changeFontSize = () => {
   textOutput.style.fontSize = `${scrollbar.value}px`;
 };
